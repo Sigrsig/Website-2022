78378dcdf66f48bc660ee7f17a4a1e81aad72096
Sigrsig <sospsig@gmail.com>
2021-12-12 20:26:38 +0100

    Colour scheme added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../img/logo.png'
+
+function Header() {
+    return (
+        <div>
+            <img src={logo} />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/TopPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TopPage() {
+    return (
+        <div className="top-page">
+            <h1>Hi! I'm Sigga</h1>
+            <h1>An Icelandic front-end engineer based in Berlin</h1>
+        </div>
+    )
+}
+
+export default TopPage
