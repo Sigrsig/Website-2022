300ec781c7e991f9da3670eea972f533330b2957
Sigrsig <sospsig@gmail.com>
2021-12-14 19:55:28 +0000

    Revert "changes added?"
    This reverts commit 3e2cc769fb3b83417fbb37f4c73264b0767f5979.

--- src/components/TopPage.js
+++ src/components/TopPage.js
@@ -7,18 +7,9 @@ function TopPage() {
     
     return (
         <div className="top-page">
-<<<<<<< Updated upstream
                 <h1 className="title">Hi! I'm Sigga</h1>
                 <h1 className="title">An Icelandic front-end engineer based in Berlin</h1>
                 
-=======
-            <div className=""></div>
-            <div className="me-intro">
-                <h1 className="text-shadow-pop-tr">Hi! I'm Sigga</h1>
-                <h1 className="text-shadow-pop-tr">An Icelandic front-end engineer based in Berlin</h1>
-            </div>
-            
->>>>>>> Stashed changes
         </div>
     )
 }
